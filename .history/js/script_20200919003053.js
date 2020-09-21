e62a8ae3f5a5107243b968ce6b57b53ff9cc9ea7
premp <premp@surveymonkey.com>
2020-09-21 16:32:42 -0400

    premonition commit

---
+++ .history/js/script_20200919003053.js
@@ -0,0 +1,31 @@
+const responsive ={
+    3
+}
+
+$(document).ready(function(){
+
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse')
+
+    // click event on toggle menu
+    $toggleCollapse.click(function(){
+        $nav.toggleClass('collapse');
+    })
+})
+
+// owl carousel for blog
+$('.owl-carousel').owlCarousel(
+    {
+        loop:true,
+        autoplay:true,
+        autoplayTimeout: 4000,
+        fluidSpeed:1,
+        dots:false,
+        nav:true,
+        animateOut:true,
+        navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')];
+        responsive:responsive
+
+
+    }
+)
---
+++ .history/js/script_20200921102945.js
@@ -0,0 +1,46 @@
+const responsive ={
+    320:{
+        items:1
+    },
+    620:{
+        items:2
+    },
+    1000:{
+        items:3
+    }
+
+}
+
+$(document).ready(function(){
+
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse')
+
+    // click event on toggle menu
+    $toggleCollapse.click(function(){
+        $nav.toggleClass('collapse');
+    })
+})
+
+// owl carousel for blog
+$('.owl-carousel').owlCarousel(
+    {
+        loop:true,
+        autoplay:true,
+        autoplayTimeout: 4000,
+        fluidSpeed:1,
+        dots:false,
+        nav:true,
+        animateOut:true,
+        navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')],
+        responsive:responsive
+    }
+)
+$(document).ready(function(){
+
+    $('.fa-arrow-circle-up').click(function(){
+        $(body).animate({
+            scrollTop:0
+        },3000)
+    })
+})
